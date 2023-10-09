02b53f9fe51e2832f0d3c5b347a15642d716fd46
José Antonio Rosales <jose.rosales.mald@gmail.com>
2023-10-09 08:19:14 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en" data-theme="dark">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Pico</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.min.css" />
+</head>
+
+<body>
+  <main class="container">
+    <article style="margin-top: 0px">
+      <p>Hola antonio</p>
+    </article>
+  </main>
+</body>
+
+</html>
+
