d0be295732857d7174f9976a93e39c98efd66b46
José Antonio Rosales <jose.rosales.mald@gmail.com>
2023-10-09 19:35:55 -0600

    feat: showing data correctly

--- index.php
+++ index.php
@@ -42,49 +42,43 @@ if ($data === null) {
       </ul>
     </nav>
     <h1 id="mb">The New York Times Best Sellers</h1>
-    <div class="grid">
-      <?php
-      $contador = 0; // Contador para llevar un registro de los libros procesados
-      foreach ($data['results']['lists'] as $list) {
-        foreach ($list['books'] as $book) {
-          if (isset($book['book_image'])) {
-            // Comienza un nuevo div cada vez que se procesan 3 libros
-            if ($contador % 3 === 0) {
-      ?>
-              <div class="grupo">
-              <?php
-            }
-            // Abre un nuevo artículo
-              ?>
-              <article id="mt">
-                <img src="<?= $book['book_image'] ?>" alt="Portada del libro">
-                <hgroup>
-                  <h3><?= $book['title'] ?></h3>
-                  <p><?= $book['author'] ?></p>
-                </hgroup>
-                <p><?= $book['description'] ?></p>
-                <footer><?= $list['list_name'] ?></footer>
-              </article>
-              <?php
-              // Cierra un artículo y un div después de procesar 3 libros
-              if ($contador % 3 === 2) {
-              ?>
-              </div>
-        <?php
-              }
-              $contador++;
-            }
-          }
+    <div>
+  <?php
+  $contador = 0; // Contador para llevar un registro de los libros procesados
+  foreach ($data['results']['lists'] as $list) {
+    foreach ($list['books'] as $book) {
+      if (isset($book['book_image'])) {
+        // Comienza un nuevo div cada vez que se procesan 3 libros
+        if ($contador % 3 === 0) {
+          echo '<div class="grid">';
         }
-        // Cierra cualquier div y artículo restantes si no son múltiplos de 3
-        while ($contador % 3 !== 0) {
-        ?>
-        <article id="mt"></article>
-      <?php
-          $contador++;
+        // Abre un nuevo artículo
+        echo '<article id="mt">';
+        echo '<img src="' . $book['book_image'] . '" alt="Portada del libro">';
+        echo '<hgroup>';
+        echo '<h3>' . $book['title'] . '</h3>';
+        echo '<p>' . $book['author'] . '</p>';
+        echo '</hgroup>';
+        echo '<p>' . $book['description'] . '</p>';
+        echo '<footer>' . $list['list_name'] . '</footer>';
+        echo '</article>';
+        $contador++;
+        // Cierra un div después de procesar 3 libros
+        if ($contador % 3 === 0) {
+          echo '</div>';
         }
-      ?>
-    </div>
+      }
+    }
+  }
+  
+  // Cierra cualquier div restante si no se alcanzan múltiplos de 3
+  while ($contador % 3 !== 0) {
+    echo '<article id="mt" style="visibility: hidden;"></article>';
+    $contador++;
+  }
+  ?>
+</div>
+
 
   </main>
 </body>
